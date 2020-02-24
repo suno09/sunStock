174c7c5137d3f0a1f8977e4a8d05709b8c0688eb
suno09 <shemsou9@gmail.com>
2020-02-24 22:04:19 +0100

    update login page

--- sun_stock/views.py
+++ sun_stock/views.py
@@ -10,13 +10,12 @@ from django.shortcuts import render
 class Index(View):
     @staticmethod
     def get(request, *args, **kwargs):
-        template_name = 'sun_stock/login.html'
+        # template_name = 'sun_stock/login.html'
+        #
+        # return render(request, template_name, {})
+        if request.session.has_key('username_session'):
+            template_name = 'sun_stock/index.html'
+        else:
+            template_name = 'sun_stock/login.html'
 
         return render(request, template_name, {})
-        # if request.session.has_key('username_session'):
-        #     # self.template_name = 'sun_stock/index.html'
-        #     return HttpResponse('API index of SunStock Management')
-        # else:
-        #     self.template_name = 'sun_stock/login.html'
-        #
-        #     return render(request, self.template_name, {})
